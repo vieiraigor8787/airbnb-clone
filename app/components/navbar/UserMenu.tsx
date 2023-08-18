6cba068033dd20ef0f6739e24731972f88998d82
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-08-18 15:17:02 -0300

    menu item

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,12 +1,17 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 import { AiOutlineMenu } from 'react-icons/Ai'
 
 import Avatar from '../Avatar'
+import MenuItem from './MenuItem'
 
 export default function UserMenu() {
-  const [isOpen, setIsOpen] = useState()
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value)
+  }, [])
 
   return (
     <div className="relative">
@@ -14,13 +19,27 @@ export default function UserMenu() {
         <div className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer">
           Home
         </div>
-        <div className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition">
+        <div
+          onClick={toggleOpen}
+          className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
+        >
           <AiOutlineMenu />
           <div className="hidden md:block">
             <Avatar />
           </div>
         </div>
       </div>
+
+      {isOpen && (
+        <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
+          <div className="flex flex-col cursor-pointer">
+            <>
+              <MenuItem onClick={() => {}} label="Login" />
+              <MenuItem onClick={() => {}} label="Novo usuário" />
+            </>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
