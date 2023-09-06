da3fc11a223e674e85b2b2339d49d602f28592a5
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-09-06 11:08:06 -0300

    router for logo

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -1,10 +1,14 @@
 'use client'
 
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 
 export default function Logo() {
+  const router = useRouter()
+
   return (
     <Image
+      onClick={() => router.push('/')}
       src="/images/Logo.png"
       height="100"
       width="100"
