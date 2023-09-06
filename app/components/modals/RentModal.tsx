8f86fc3f84b764da2dee9016d127342e0c5571de
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-09-06 12:25:28 -0300

    config categories on home modal

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -1,9 +1,11 @@
 'use client'
 
 import { useMemo, useState } from 'react'
+import { FieldValues, useForm } from 'react-hook-form'
 
 import useRentModal from '@/hooks/useRentModal'
 import { categories } from '../navbar/Categories'
+import CategoryInput from '../inputs/CategoryInput'
 import Modal from './Modal'
 import Heading from '../Heading'
 
@@ -21,6 +23,37 @@ export default function RentModal() {
 
   const [step, setStep] = useState(STEPS.CATEGORY)
 
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm<FieldValues>({
+    defaultValues: {
+      category: '',
+      location: null,
+      guestCount: 1,
+      roomCount: 1,
+      bathroomCount: 1,
+      imageSrc: '',
+      price: 1,
+      title: '',
+      description: '',
+    },
+  })
+
+  const category = watch('category')
+
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldDirty: true,
+      shouldTouch: true,
+      shouldValidate: true,
+    })
+  }
+
   const onBack = () => {
     setStep((value) => value - 1)
   }
@@ -29,15 +62,15 @@ export default function RentModal() {
   }
 
   const actionLabel = useMemo(() => {
-    if (step === STEPS.PRICE) return 'Create'
+    if (step === STEPS.PRICE) return 'Criar'
 
-    return 'Next'
+    return 'Próximo'
   }, [step])
 
   const secondaryActionLabel = useMemo(() => {
     if (step === STEPS.CATEGORY) return undefined
 
-    return 'Back'
+    return 'Voltar'
   }, [step])
 
   let bodyContent = (
@@ -47,9 +80,14 @@ export default function RentModal() {
         subtitle="Escolha uma categoria"
       />
       <div className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto">
-        {categories.map((category) => (
-          <div key={category.label} className="col-span-1">
-            {category.label}
+        {categories.map((item) => (
+          <div key={item.label} className="col-span-1">
+            <CategoryInput
+              icon={item.icon}
+              label={item.label}
+              onClick={(category) => setCustomValue('category', category)}
+              selected={category === item.label}
+            />
           </div>
         ))}
       </div>
