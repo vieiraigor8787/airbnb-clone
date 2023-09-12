ae60682b359ad7c2f36958dcc911f3db9ff5d08c
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-09-12 11:34:11 -0300

    setting component for cards of listings

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import getCurrentUser from './actions/getCurrentUser'
 import getListings from './actions/getListings'
 import ClientOnly from './components/ClientOnly'
 import Container from './components/Container'
 import EmptyState from './components/EmptyState'
+import ListingCard from './components/listings/ListingCard'
 
 export default async function Home() {
   const listings = await getListings()
+  const currentUser = await getCurrentUser()
 
   if (listings.length === 0) {
     return (
@@ -19,7 +22,13 @@ export default async function Home() {
       <Container>
         <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
           {listings.map((listing: any) => {
-            return <div>{listing.title}</div>
+            return (
+              <ListingCard
+                key={listing.id}
+                data={listing}
+                currentUser={currentUser}
+              />
+            )
           })}
         </div>
       </Container>
