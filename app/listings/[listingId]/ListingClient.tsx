d68bdd5ae2d53dc94d1044f5e7942a0e0268c8ed
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-09-13 17:36:55 -0300

    date range and total price

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -1,13 +1,24 @@
 'use client'
 
+import axios from 'axios'
+import toast from 'react-hot-toast'
+import { useRouter } from 'next/navigation'
 import { Reservation } from '@prisma/client'
-import { useMemo } from 'react'
+import { useCallback, useMemo, useState } from 'react'
+import { eachDayOfInterval, setDate } from 'date-fns'
 
 import { SafeListing, SafeUser } from '@/app/types'
 import { categories } from '@/app/components/navbar/Categories'
 import Container from '@/app/components/Container'
 import ListingHead from '@/app/components/listings/ListingHead'
 import ListingInfo from '@/app/components/listings/ListingInfo'
+import useLoginModal from '@/app/hooks/useLoginModal'
+
+const initialDateRange = {
+  startDate: new Date(),
+  endDate: new Date(),
+  key: 'selection',
+}
 
 interface ListingClientProps {
   reservations?: Reservation[]
@@ -20,8 +31,56 @@ interface ListingClientProps {
 export default function ListingClient({
   listing,
   currentUser,
-  reservations,
+  reservations = [],
 }: ListingClientProps) {
+  const loginModal = useLoginModal()
+  const router = useRouter()
+
+  const disabledDates = useMemo(() => {
+    let dates: Date[] = []
+
+    reservations.forEach((reservation) => {
+      const range = eachDayOfInterval({
+        start: new Date(reservation.startDate),
+        end: new Date(reservation.endDate),
+      })
+
+      dates = [...dates, ...range]
+    })
+
+    return dates
+  }, [reservations])
+
+  const [isLoading, setIsLoading] = useState(false)
+  const [totalPrice, setTotalPrice] = useState(listing.price)
+  const [dateRange, setDateRange] = useState(initialDateRange)
+
+  const onCreateReservation = useCallback(() => {
+    if (!currentUser) return loginModal.onOpen()
+
+    setIsLoading(true)
+
+    axios
+      .post('api/reservations', {
+        totalPrice,
+        startDate: dateRange.startDate,
+        endDate: dateRange.endDate,
+        listingId: listing?.id,
+      })
+      .then(() => {
+        toast.success('Reserva realizada com sucesso')
+        setDateRange(initialDateRange)
+        //redirect to /trips
+        router.refresh()
+      })
+      .catch(() => {
+        toast.error('Algo deu errado')
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
+  }, [totalPrice, dateRange, listing?.id, router, currentUser, loginModal])
+
   const category = useMemo(() => {
     return categories.find((item) => item.label === listing.category)
   }, [listing.category])
