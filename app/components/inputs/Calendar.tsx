77a2aaccf99857f022f4f2e123f478f16bb90c83
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-09-13 18:19:50 -0300

    calendar component

--- app/components/inputs/Calendar.tsx
+++ app/components/inputs/Calendar.tsx
@@ -2,16 +2,30 @@
 
 import { DateRange, Range, RangeKeyDict } from 'react-date-range'
 
+import 'react-date-range/dist/styles.css'
+import 'react-date-range/dist/theme/default.css'
+
 interface CalendarProps {
   value: Range
   onChange: (value: RangeKeyDict) => void
-  disabledDate?: Date[]
+  disabledDates?: Date[]
 }
 
 export default function Calendar({
-  disabledDate,
+  disabledDates,
   value,
   onChange,
 }: CalendarProps) {
-  return <DateRange />
+  return (
+    <DateRange
+      rangeColors={['#262626']}
+      ranges={[value]}
+      date={new Date()}
+      onChange={onChange}
+      direction="vertical"
+      showDateDisplay={false}
+      minDate={new Date()}
+      disabledDates={disabledDates}
+    />
+  )
 }
